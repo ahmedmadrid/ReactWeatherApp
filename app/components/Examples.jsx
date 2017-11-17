ede819f97dc9df609e071602e36282952da9c2f2
Silver <ahmed1992madrid@gmail.com>
2017-11-17 17:46:09 +0300

    Add frid system & style examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-centered">Examples</h1>
+      <p>Here are a few examples locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/location=Baghdad'>Baghdad</Link>
+        </li>
+        <li>
+          <Link to='/?location=Dubai'>Dubai</Link>
+        </li>
+      </ol>
     </div>
   )
 };
